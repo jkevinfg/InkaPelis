bd253aa1940442642be4ea1229a95ba80f507878
JairKevinFG <jkfloresg@uni.pe>
2021-10-17 17:24:03 -0500

    cache

--- backend/lib/mongo.js
+++ backend/lib/mongo.js
@@ -1,60 +1,75 @@
-const {MongoClient, ObjectId }  = require('mongodb');
-const { config }  = require('../config');
+const { MongoClient, ObjectId } = require('mongodb');
+const { config } = require('../config');
 
-const user = encodeURIComponent(config.dbUser);
-const password = encodeURIComponent(config.dbPassword);
-const db_name = config.dbName;
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const DB_NAME = config.dbName;
 
-const MONGO_URI = `mongodb+srv://${user}:${password}@${config.dbHost}/${db_name}?retryWrites=true&w=majority`;
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}/${DB_NAME}?retryWrites=true&w=majority`;
 
 class MongoLib {
     constructor() {
-        this.client = new MongoClient(MONGO_URI, { useNewUrlParser : true});
-        this.dbName = db_name;
-    }
-    connect(){
-        if(!MongoLib.connection) {
-            MongoLib.connection = new Promise ((resolve, reject ) => {
-                this.client.connect(err => {
-                    if(err){
-                        reject(err);
-                    }
-                    console.log('connected succesfully to mongo');
-                    resolve(this.client.db(this.dbName));
-                })
-            })
-        }else {
-            return MongoLib.connection;
-        }
-    }
-    getAll(collection,query){
-        return this.connect().then(db => { //la promesa nos devuelve una instancia  a la base de datos, esa instancia tiene los metodos de mongo
-            return db.collection(collection).find(query).toArray();
-        })
+        this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
+        this.dbName = DB_NAME;
     }
 
-    get(collection,id){
-        return this.connect().then(db => { //la promesa nos devuelve una instancia  a la base de datos
-            return db.collection(collection).findOne({_id : ObjectId(id)})
-        })
+    connect() {
+        if (!MongoLib.connection) {
+            MongoLib.connection = new Promise((resolve, reject) => {
+                this.client.connect(err => {
+                    if (err) {
+                        reject(err);
+                    }
+
+                    console.log('Connected succesfully to mongo');
+                    resolve(this.client.db(this.dbName));
+                });
+            });
+        }
+
+        return MongoLib.connection;
     }
-    create(collection , data){
+
+    getAll(collection, query) {
+        return this.connect().then(db => {
+            return db
+                .collection(collection)
+                .find(query)
+                .toArray();
+        });
+    }
+
+    get(collection, id) {
+        return this.connect().then(db => {
+            return db.collection(collection).findOne({ _id: ObjectId(id) });
+        });
+    }
+
+    create(collection, data) {
         return this.connect()
-        .then(db => { //la promesa nos devuelve una instancia  a la base de datos, esa instancia tiene los metodos de mongo
-            return db.collection(collection).insertOne(data)
-        }).then(result => result.insertedId)
+            .then(db => {
+                return db.collection(collection).insertOne(data);
+            })
+            .then(result => result.insertedId);
     }
-    update(collection , id , data){
-        return this.connect().then(db => { //la promesa nos devuelve una instancia  a la base de datos , esa instancia tiene los metodos de mongo
-            return db.collection(collection).updateOne({_id : ObjectId(id)} , {$set : data }, {upsert : true})
-        }).then(result => result.upsertedId || id)
+
+    update(collection, id, data) {
+        return this.connect()
+            .then(db => {
+                return db
+                    .collection(collection)
+                    .updateOne({ _id: ObjectId(id) }, { $set: data }, { upsert: true });
+            })
+            .then(result => result.upsertedId || id);
     }
-    delete(collection , id){
-        return this.connect().then(db => { //la promesa nos devuelve una instancia  a la base de datos, esa instancia tiene los metodos de mongo
-            return db.collection(collection).deleteOne({_id : ObjectId(id)})
-        }).then( () => id)
+
+    delete(collection, id) {
+        return this.connect()
+            .then(db => {
+                return db.collection(collection).deleteOne({ _id: ObjectId(id) });
+            })
+            .then(() => id);
     }
 }
 
 module.exports = MongoLib;
-
