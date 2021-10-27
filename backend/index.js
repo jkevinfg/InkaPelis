c3c3210a306751eeef36edb219b4f235a7b3b716
JairKevinFG <jkfloresg@uni.pe>
2021-10-26 19:27:01 -0500

    terminado backend

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,31 @@
 const express = require('express');
 const app = express();
-const {config} = require('./config/index');
-const moviesApi = require('./routes/movies');
 
-const { logErrors, errorHandler, wrapErrors} = require('./utils/middleware/errorHandlers')
+const { config } = require('./config/index');
+const moviesApi = require('./routes/movies.js');
+
+const {
+    logErrors,
+    wrapErrors,
+    errorHandler
+} = require('./utils/middleware/errorHandlers.js');
 
 const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
-
-
-//body parser
+// body parser
 app.use(express.json());
 
+// routes
 moviesApi(app);
 
-//catch 404
+// Catch 404
 app.use(notFoundHandler);
 
-//Errores middleware
+// Errors middleware
 app.use(logErrors);
 app.use(wrapErrors);
 app.use(errorHandler);
 
-
-
-app.listen(config.port  , () => {
-    console.log(`Server on port ${config.port}`)
-} )
-
+app.listen(config.port, function () {
+    console.log(`Listening http://localhost:${config.port}`);
+});
--- backend/services/movies.js
+++ backend/services/movies.js
@@ -1,33 +1,40 @@
 const MongoLib = require('../lib/mongo');
 
 class MoviesService {
-    constructor(){
+    constructor() {
         this.collection = 'movies';
         this.mongoDB = new MongoLib();
     }
-    async getMovies({tags}){
-        const query = tags && {tags : {$in : tags}};
+
+    async getMovies({ tags }) {
+        const query = tags && { tags: { $in: tags } };
         const movies = await this.mongoDB.getAll(this.collection, query);
         return movies || [];
     }
-    async getMovie ({movieId}){
+
+    async getMovie({ movieId }) {
         const movie = await this.mongoDB.get(this.collection, movieId);
-        return movie || [];
+        return movie || {};
     }
-    async createMovie ({movie}){
+
+    async createMovie({ movie }) {
         const createMovieId = await this.mongoDB.create(this.collection, movie);
         return createMovieId;
     }
 
-    async updateMovie({movieId , movie} = {}){
-        const updatedMovieId = await this.mongoDB.update(this.collection,movieId, movie);
+    async updateMovie({ movieId, movie } = {}) {
+        const updatedMovieId = await this.mongoDB.update(
+            this.collection,
+            movieId,
+            movie
+        );
         return updatedMovieId;
     }
-    async deleteMovie({movieId}){
+
+    async deleteMovie({ movieId }) {
         const deletedMovieId = await this.mongoDB.delete(this.collection, movieId);
         return deletedMovieId;
     }
 }
 
 module.exports = MoviesService;
-
