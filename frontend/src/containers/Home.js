be33fecef4d00d58c03eb6e9b00a2ec950eca0df
JairKevinFG <jkfloresg@uni.pe>
2021-11-28 10:55:30 -0500

    iniciando redux

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,40 +1,39 @@
 import React from 'react';
-import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
-import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
+import { connect } from 'react-redux';
 
-const API = 'http://localhost:3000/initialState';
+const Home = ({ myList, trends, originals }) => {
 
-const Home = () => {
-  const initialState = useInitialState(API);
-
-  console.log(initialState);
-  return initialState.length === 0 ? <h1>Loading</h1> : (
+  return (
 
     <React.Fragment>
       <Search />
-      {initialState.mylist.length > 0 && (
-        <Categories title='Mi Lista'>
+      {myList.length > 0 && (
+        <Categories title='Mi lista'>
           <Carousel>
-            {initialState.mylist.map(item => <CarouselItem key={item.id} {...item} />)}
+            {myList.map(item => (
+              <CarouselItem
+                key={item.id}
+                {...item}
+                isList
+              />
+            ))}
           </Carousel>
         </Categories>
       )}
-
       <Categories title='Tendencias'>
         <Carousel>
-          {initialState.trends.map(item => <CarouselItem key={item.id} {...item} />)}
+          {trends.map(item => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
 
-      <Categories title='Originales'>
+      <Categories title='Originales de Platzi Video'>
         <Carousel>
-          { initialState.originals.map(item => <CarouselItem key={item.id} {...item} />)}
+          {originals.map(item => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
 
@@ -44,4 +43,12 @@ const Home = () => {
 
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+
+export default connect(mapStateToProps, null)(Home);
