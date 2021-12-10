3abfac47d645e5fa7a3f624846c5c14caa17c647
JairKevinFG <jkfloresg@uni.pe>
2021-12-10 10:54:32 -0500

    servir react

--- frontend/src/server/server.js
+++ frontend/src/server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import webpack from 'webpack';
 
 dotenv.config();
 
@@ -8,11 +9,32 @@ const app = express();
 
 if (ENV === 'development') {
   console.log('Development config');
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig);
+  // const serverConfig = { port: PORT, hot: true };
+  const serverConfig = { serverSideRender: true };
+
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webpackHotMiddleware(compiler));
 
 }
 
 app.get('*', (req, res) => {
-  res.send({ hello: 'express' });
+  res.send(`
+  <!DOCTYPE html>
+    <html>
+      <head>
+        <link rel="stylesheet" href="frontend/assets/app.css" type="text/css">
+        <title>Platzi Video</title>
+      </head>
+      <body>
+        <div id="app"></div>
+        <script src="assets/app.js" type="text/javascript"></script>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(PORT, (err) => {
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/frontend/index.js', 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true'],
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'assets/app.js',
     publicPath: '/',
   },
   resolve: {
@@ -23,40 +24,43 @@ module.exports = {
       },
       {
         test: /\.html$/,
-        use: {
-          loader: 'html-loader',
-        },
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
       },
       {
         test: /\.(s*)css$/,
         use: [
-          { loader: MiniCssExtractPlugin.loader },
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
           'css-loader',
           'sass-loader',
         ],
       },
       {
-        test: /\.(png|gif|jpg|svg)$/,
+        test: /\.(png|gif|jpg)$/,
         use: [
           {
-            loader: 'file-loader',
-            options: { name: 'assets/[hash].[ext]' },
+            'loader': 'file-loader',
+            options: {
+              name: 'frontend/assets/[hash].[ext]',
+            },
           },
         ],
       },
     ],
   },
   devServer: {
-    disableHostCheck: true,
     historyApiFallback: true,
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: './index.html',
-    }),
+    new webpack.HotModuleReplacementPlugin(),
+
     new MiniCssExtractPlugin({
-      filename: 'assets/[name].css',
+      filename: 'frontend/assets/app.css',
     }),
   ],
 };
