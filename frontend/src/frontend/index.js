d4844b635b89db2e17244bc857690f53611b731e
jkevinfg <jkfloresg@uni.pe>
2021-12-27 16:21:35 -0500

    redux thunk

--- frontend/src/frontend/index.js
+++ frontend/src/frontend/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, compose } from 'redux';
 import { Router } from 'react-router';
 import { createBrowserHistory } from 'history';
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import reducer from './reducers';
 import App from './routes/App';
 
-const history = createBrowserHistory();
-const preloadedState = window.__PRELOADED_STATE__;
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, preloadedState, composeEnhancers());
+const preloadedState = window.__PRELOADED_STATE__;
+const store = createStore(reducer, preloadedState, composeEnhancers(applyMiddleware(thunk)));
+const history = createBrowserHistory();
 
 delete window.__PRELOADED_STATE__;
 
@@ -20,5 +21,5 @@ ReactDOM.hydrate(
       <App />
     </Router>
   </Provider>,
-  document.getElementById('app'),
+  document.getElementById('app')
 );
--- frontend/src/server/server.js
+++ frontend/src/server/server.js
@@ -13,10 +13,23 @@ import reducer from '../frontend/reducers';
 import initialState from '../frontend/initialState';
 import getManifest from './getManifest';
 
+import cookieParser from 'cookie-parser';
+import boom from '@hapi/boom';
+import passport from 'passport';
+import axios from 'axios';
 dotenv.config();
 
-const { ENV, PORT } = process.env;
+
 const app = express();
+const { ENV, PORT } = process.env;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./utils/auth/strategies/basic');
+
 
 if (ENV === 'development') {
   console.log('Development config');
@@ -74,6 +87,49 @@ const renderApp = (req, res) => {
 
   res.send(setResponse(html, preloadedState, req.hashManifest));
 };
+
+app.post("/auth/sign-in", async function(req, res, next) {
+  passport.authenticate("basic", function(error, data) {
+    try {
+      if (error || !data) {
+        next(boom.unauthorized());
+      }
+
+      req.login(data, { session: false }, async function(error) {
+        if (error) {
+          next(error);
+        }
+
+        const { token, ...user } = data;
+
+        res.cookie("token", token, {
+          httpOnly: !config.dev,
+          secure: !config.dev
+        });
+
+        res.status(200).json(user);
+      });
+    } catch (error) {
+      next(error);
+    }
+  })(req, res, next);
+});
+
+app.post("/auth/sign-up", async function(req, res, next) {
+  const { body: user } = req;
+
+  try {
+    await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: "post",
+      data: user
+    });
+
+    res.status(201).json({ message: "user created" });
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.get('*', renderApp);
 
